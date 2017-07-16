0b4f996f3f28b12a8ef2bdf795ad7ae77505a058
zinderud <mokosam@gmail.com>
2017-07-16 13:29:09 +0300

    yukle-db

--- db/yukle-db.ts
+++ db/yukle-db.ts
@@ -9,10 +9,6 @@ initializeApp(environment.firebaseConfig);
 
 
 const kitaplarRef = database().ref('kitaplar');
-const bolumlerRef = database().ref('bolumler');
-
-
-
 dbData.kitaplar.forEach( kitap => {
 
   console.log('add kitap', kitap.url);
@@ -23,6 +19,24 @@ dbData.kitaplar.forEach( kitap => {
       acıklama: kitap.acıklama 
   });
 
+
+
+/* 
+veritabanında yapmak istediğimiz 
+"kitaplar" : {
+    "-Kp7p-YCh97CuRmSrzKv" : {
+      "acıklama" : "İkbal'in olgunluk dönemi eserlerinden olan Cavitname, bu büyük düşünüründüşünce silsilesinin devamı ve en önemli dönüm noktası durumundadır.",
+      "url" : "cavidname",
+      "yazar" : "Muhammed ikbal"
+    },
+    "-Kp7p-YLKAafgquZvqQX" : {
+      "acıklama" : "Mevlananın 6 ciltten oluşan ....",
+      "url" : "mesnevi",
+      "yazar" : "Mevlana"
+    }
+  } */
+
+  const bolumlerRef = database().ref('bolumler');
   let KitapKeyHerBolum = [];
 
   kitap.bolumler.forEach((bolum:any) =>  {
@@ -36,7 +50,28 @@ dbData.kitaplar.forEach( kitap => {
       }).key);
 
   });
-
+/*  "bolumler" : {
+    "-Kp7p-YH8StTIcPP9G4Y" : {
+      "bolum_acıklama" : "cavidname bolum1 acıklama",
+      "sayfa" : "2:49",
+      "tags" : "bol1"
+    },
+    "-Kp7p-YJpbELBSFtDhou" : {
+      "bolum_acıklama" : "cavidname bolum2 acıklama",
+      "sayfa" : "22:49",
+      "tags" : "bol2"
+    },
+    "-Kp7p-YMgF0ito4PdO9i" : {
+      "bolum_acıklama" : "mesnevi bolum1 acıklama",
+      "sayfa" : "2:49",
+      "tags" : "bol1"
+    },
+    "-Kp7p-YNAftMpeVwD_ge" : {
+      "bolum_acıklama" : "mesnevi bolum2 acıklama",
+      "sayfa" : "22:49",
+      "tags" : "bol2"
+    }
+  }, */
 
   const association = database().ref('KitapHerBolum');
 
@@ -48,6 +83,16 @@ dbData.kitaplar.forEach( kitap => {
     KitapBolumAssociation.set(true);
   });
 
+/*    "KitapHerBolum" : {
+    "-Kp7p-YCh97CuRmSrzKv" : {
+      "-Kp7p-YH8StTIcPP9G4Y" : true,
+      "-Kp7p-YJpbELBSFtDhou" : true
+    },
+    "-Kp7p-YLKAafgquZvqQX" : {
+      "-Kp7p-YMgF0ito4PdO9i" : true,
+      "-Kp7p-YNAftMpeVwD_ge" : true
+    }
+  }, */
 
 });
 
